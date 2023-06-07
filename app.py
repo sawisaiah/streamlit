ca7f83982014c1dd3fcd8b9e110b585addba0b04
sawisaiah <99721797+sawisaiah@users.noreply.github.com>
2023-06-07 16:23:00 +0630

    Update app.py

--- app.py
+++ app.py
@@ -1,20 +1,21 @@
-
 import streamlit as st
 
-def calculate_bmi(height, weight):
-    bmi = weight / (height / 100) ** 2
+def calculate_bmi(height_ft, height_in, weight_lb):
+    height = (height_ft * 12) + height_in
+    bmi = (weight_lb / (height ** 2)) * 703
     return bmi
 
 # Streamlit App
 st.title("BMI Calculator")
 
 # Input fields for height and weight
-height = st.number_input("Enter your height (in cm):", min_value=1.0, step=0.1)
-weight = st.number_input("Enter your weight (in kg):", min_value=1.0, step=0.1)
+height_ft = st.number_input("Enter your height (feet):", min_value=1, value=5)
+height_in = st.number_input("Enter your height (inches):", min_value=0, max_value=11, value=6)
+weight_lb = st.number_input("Enter your weight (pounds):", min_value=1, value=150)
 
 # Calculate BMI and display the result
 if st.button("Calculate BMI"):
-    bmi = calculate_bmi(height, weight)
+    bmi = calculate_bmi(height_ft, height_in, weight_lb)
     st.write("Your BMI is {:.2f}".format(bmi))
 
     # Provide interpretation of BMI category
